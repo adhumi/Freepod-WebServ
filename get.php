50981990ca808c0488a433f54c310b3ca17303a7
Adrien Humilière <adhumi@gmail.com>
2012-02-19 19:24:08 +0100

    Ajout authentification par API Key

--- get.php
+++ get.php
@@ -1,28 +1,31 @@
-<?php 
+<?php
 
-include('bdd_connect.php'); 
+include ('bdd_connect.php');
 
 connexion ( 'webserv' );
 
+if (isset ( $_GET ['key'] ) && !mysql_query ( "SELECT * FROM api_key WHERE key='" . $_GET ['key'] . "'" ) ) {
+	
+	if (isset ( $_GET ['podcasts'] )) {
+		$query = "SELECT * FROM podcasts";
+		$sth = mysql_query ( $query );
+		$rows = array ();
+		while ( $r = mysql_fetch_assoc ( $sth ) ) {
+			$rows [] = $r;
+		}
+		echo json_encode ( $rows );
+	} 
 
-if (isset ( $_GET ['podcasts'] )) {
-	$query = "SELECT * FROM podcasts";
-	$sth = mysql_query ( $query );
-	$rows = array ();
-	while ( $r = mysql_fetch_assoc ( $sth ) ) {
-		$rows [] = $r;
+	else if (isset ( $_GET ['episodes'] )) {
+		$query = "SELECT * FROM episodes WHERE id_podcast = " . $_GET ['episodes'];
+		$sth = mysql_query ( $query );
+		$rows = array ();
+		while ( $r = mysql_fetch_assoc ( $sth ) ) {
+			$rows [] = $r;
+		}
+		echo json_encode ( $rows );
 	}
-	print json_encode ( $rows );
-} 
-
-else if (isset ( $_GET ['episodes'] )) {
-	$query = "SELECT * FROM episodes WHERE id_podcast = " . $_GET['episodes'];
-	$sth = mysql_query ( $query );
-	$rows = array ();
-	while ( $r = mysql_fetch_assoc ( $sth ) ) {
-		$rows [] = $r;
-	}
-	print json_encode ( $rows );
+} else {
+	echo "{\"erreur\":\"API Key\"}";
 }
-
 ?>
