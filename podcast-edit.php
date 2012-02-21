d9e6cc7fe8b7769fd30b08d1d5394a09f4b647b1
Adrien Humilière <adhumi@gmail.com>
2012-02-21 08:45:35 +0100

    Dernier commit avant d'aller bosser (podcast-edit.php)
    Nerd...

--- podcast-edit.php
+++ podcast-edit.php
@@ -21,12 +21,15 @@ while ( $row = mysql_fetch_row ( $result ) ) {
 				<li><a href="index2.php"><i class="icon-home"></i>Home</a></li>
 
 				<li class="nav-header">Outils</li>
-				<lili><a href="podcasts.php"><i
-						class="icon-th-list"></i> Tous les podcasts</a></li>
+				<li>
+				<a href="podcasts.php"><i class="icon-th-list"></i> Tous les
+					podcasts</a>
+				</li>
 				<li><a href="episodes.php"><i class="icon-music"></i> Tous les
 						&eacute;pisodes</a></li>
 				<li><a href="#"><i class="icon-plus"></i> Ajout d'un podcast</a></li>
 				<li><a href="#"><i class="icon-signal"></i> Statistiques</a></li>
+			
 			</ul>
 		</div>
 		<div class="span9">
@@ -35,9 +38,10 @@ while ( $row = mysql_fetch_row ( $result ) ) {
 			</div>
 			<ul class="breadcrumb">
 				<li><a href="index2.php">Home</a> <span class="divider">/</span></li>
-				<li><a href="podcasts.php">Tous les podcasts</a> <span class="divider">/</span></li>
-				<li><a href="podcast.php?id=<?php echo $row[0]; ?>"><?php echo $row[1]; ?></a> <span
+				<li><a href="podcasts.php">Tous les podcasts</a> <span
 					class="divider">/</span></li>
+				<li><a href="podcast.php?id=<?php echo $row[0]; ?>"><?php echo $row[1]; ?></a>
+					<span class="divider">/</span></li>
 				<li class="active">Edit</li>
 			</ul>
 
@@ -47,8 +51,8 @@ while ( $row = mysql_fetch_row ( $result ) ) {
 					<div class="control-group">
 						<label class="control-label" for="titre">Titre</label>
 						<div class="controls">
-							<input type="text" class="input-xlarge" id="titre" name="titre"
-								value="<?php echo $row[1]; ?>">
+							<input type="text" class="input-xlarge span5" id="titre"
+								name="titre" value="<?php echo $row[1]; ?>">
 						</div>
 					</div>
 					<div class="control-group">
@@ -63,22 +67,31 @@ while ( $row = mysql_fetch_row ( $result ) ) {
 					<div class="control-group">
 						<label class="control-label" for="url_rss">Flux RSS (URL)</label>
 						<div class="controls">
-							<input type="text" class="input-xlarge" id="url_rss"
-								name="url_rss" value="<?php echo $row[2]; ?>">
+							<div class="input-prepend">
+								<span class="add-on">http://</span> <input type="text"
+									class="input-xlarge span5" id="url_rss" name="url_rss"
+									value="<?php echo $row[2]; ?>">
+							</div>
 						</div>
 					</div>
 					<div class="control-group">
 						<label class="control-label" for="url_site">Site du podcast (URL)</label>
 						<div class="controls">
-							<input type="text" class="input-xlarge" id="url_site"
+							<div class="input-prepend">
+								<span class="add-on">http://</span> 
+								<input type="text" class="input-xlarge span5" id="url_site"
 								name="url_site" value="<?php echo $row[5]; ?>">
+							</div>
 						</div>
 					</div>
 					<div class="control-group">
 						<label class="control-label" for="url_freepod">Page Freepod (URL)</label>
 						<div class="controls">
-							<input type="text" class="input-xlarge" id="url_freepod"
+						<div class="input-prepend">
+								<span class="add-on">http://</span> 
+							<input type="text" class="input-xlarge span5" id="url_freepod"
 								name="url_freepod" value="<?php echo $row[6]; ?>">
+							</div>
 						</div>
 					</div>
 					<div class="control-group">
@@ -94,13 +107,14 @@ while ( $row = mysql_fetch_row ( $result ) ) {
 					<div class="control-group">
 						<label class="control-label" for="logo">Logo normal</label>
 						<div class="controls">
-							<input type="text" class="input-xlarge" id="logo"
-								name="logo" value="<?php echo $row[9]; ?>">
+							<input type="text" class="input-xlarge" id="logo" name="logo"
+								value="<?php echo $row[9]; ?>">
 						</div>
 					</div>
 					<div class="control-group">
 						<div class="controls">
-							<input type="submit" class="btn-primary btn-large" value="Enregistrer">
+							<input type="submit" class="btn-primary btn-large"
+								value="Enregistrer">
 						</div>
 					</div>
 				</fieldset>
